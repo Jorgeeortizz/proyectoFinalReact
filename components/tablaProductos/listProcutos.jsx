231ec38645ea58da920206374c49a3bd19c176aa
Jorge Ortiz <ortizzjorgee@gmail.com>
2024-12-04 18:26:37 -0300

    preparando nuevo repo

---
+++ components/tablaProductos/listProcutos.jsx
@@ -0,0 +1,5 @@
+import {box, CircularProgress, Tab, TableContainer, Toolbar} From "@mui/material",
+import { useEffect, useState } from "react";
+import { booMockup } from ".../../src/utils/producsMockup;
+
+export const 
